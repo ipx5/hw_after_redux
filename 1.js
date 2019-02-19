84b8539bdbdbd910032f87fc2ac55c5f613120d4
ipx5 <polyakovx5@gmail.com>
2019-02-19 21:25:16 +0300

    sec commit

--- 1.js
+++ 1.js
@@ -1,31 +1,17 @@
 'use strict';
 
-// function getFrequency(s,n) {
-//     var freq = {}; 
-//     for (var i=0; i<s.length;i++) {
-//         var character = s.charAt(i);
-//         if (freq[character]) {
-//            freq[character]++;
-//         } else {
-//            freq[character] = 1;
-//         }
-//     }
-//     return freq;
-// };
-
 function getFrequency(s,n) {
         var freq = {}; 
-        var sn = s.repeat(n).substring(0, n);
+        var sn = s.repeat(n/s.length+1).substring(0, n);
         for (var i=0; i<sn.length;i++) {
             var char = sn.charAt(i);
             if (freq[char]) {
                 freq[char]++;
            } else {
-                freq[char] = 1;
+                freq[char]= 1 ;
             } 
         }
-        return freq;
+        return freq["a"];
 }
-
-    
-console.log(getFrequency('x', 100))
+ 
+console.log(getFrequency('aba', 10))
